4d1fdc9f31e95b9f5ec66b479008a5b4f34fc715
Guilherme Carvalho <guilhermefreitas117@gmail.com>
2025-09-16 17:06:01 -0300

    About section mobile optimization

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,38 +9,38 @@ gsap.registerPlugin(ScrollTrigger);
 
 const About = () => { 
     useGSAP(() => {
-        gsap.from(gsap.utils.toArray('.about-card'), {
+        gsap.utils.toArray('.about-card').forEach((card, i) => {
+        gsap.from(card, {
             scrollTrigger: {
-                trigger: '#cards',
-                start: 'center 90%',
-                end: 'bottom 50%',
-                scrub: true,
+            trigger: card,
+            start: 'top 90%',
             },
+            duration: 1,
             opacity: 0,
             y: -150,
-            stagger: 0.1,
-            ease: 'power3.inOut'
-        })
-        
+            delay: i * 0.1,
+            ease: 'power3.inOut',
+        });
+        });
     }, [])
 
   return (
-    <div className='grid grid-cols-12 px-8 gap-8'>
+    <div className='grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-2 md:gap-4 lg:gap-8 px-2 md:px-4 lg:px-8'>
         
-        <TextScroll className="col-span-9 mt-64 text-6xl leading-20">
+        <TextScroll className="col-span-3 md:col-span-6 lg:col-span-9 mt-64 text-[clamp(2rem,5vw,60px)] leading-snug">
             Vestibulum nunc ipsum, hendrerit quis laoreet vitae, molestie nec sem. Donec eleifend arcu sed justo elementum fermentum.
         </TextScroll>
 
-        <div id='cards' className="col-span-12 grid grid-cols-12 gap-8">
+        <div id='cards' className="col-span-3 md:col-span-7 lg:col-span-12 grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-8 mt-64">
             <AboutCard className="about-card col-start-1" imageSrc={'/star.svg'} imageAlt={'Beneficio 1'} title={'Beneficio 1'}>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo orci eu turpis consequat, eget mollis elit luctus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
             </AboutCard>
 
-            <AboutCard className="about-card col-start-5" imageSrc={'/square.svg'} imageAlt={'Beneficio 2'} title={'Beneficio 2'}>
+            <AboutCard className="about-card col-start-1 lg:col-start-5 pt-16 lg:pt-0" imageSrc={'/square.svg'} imageAlt={'Beneficio 2'} title={'Beneficio 2'}>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo orci eu turpis consequat, eget mollis elit luctus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
             </AboutCard>
 
-            <AboutCard className="about-card col-start-9" imageSrc={'/sun.svg'} imageAlt={'Beneficio 3'} title={'Beneficio 3'}>
+            <AboutCard className="about-card col-start-1 lg:col-start-9 pt-16 lg:pt-0" imageSrc={'/sun.svg'} imageAlt={'Beneficio 3'} title={'Beneficio 3'}>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo orci eu turpis consequat, eget mollis elit luctus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
             </AboutCard>
         </div>
--- src/components/about/AboutCard.jsx
+++ src/components/about/AboutCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const AboutCard = ({imageSrc, imageAlt, title, children, className}) => {
   return (
-    <div className={`col-span-3 mt-64 ${className}`}>
+    <div className={`col-span-full lg:col-span-3 ${className}`}>
         <div className="flex gap-8 items-center pb-8">
             <Image src={imageSrc} alt={imageAlt} width={50} height={50}/>
             <span className='text-3xl'>{title}</span>
