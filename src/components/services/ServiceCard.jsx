e9f65320971976034c03835e6b9d78bb88c10cb5
Guilherme Carvalho <guilhermefreitas117@gmail.com>
2025-09-23 16:31:57 -0300

    Minor animations change

--- src/components/services/ServiceCard.jsx
+++ src/components/services/ServiceCard.jsx
@@ -43,7 +43,7 @@ const ServiceCard = ({num, src, alt, title, paragraph, b1, b2, b3}) => {
   return (
     <div>
         <div className="service-card bg-background border-b border-[#00659840] transition-colors duration-500 ease-in-out hover:bg-[#D9F2FF] cursor-pointer py-8">
-            <div className="custom-grid items-center">
+            <div className="custom-grid">
                 <span className='text-[clamp(2rem,5vw,60px)] col-span-3 md:col-span-1'>{num}</span>
                 <div className='image-container col-start-1 lg:col-start-2 col-span-3 overflow-hidden'>
                     <Image src={src} alt={alt} width={1920} height={1080} className='img aspect-square object-cover rounded-xl'/>
