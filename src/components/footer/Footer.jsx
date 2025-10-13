96f12d711e6d7d791ba36b985ea2dda49e3d1db3
Guilherme Carvalho <guilhermefreitas117@gmail.com>
2025-10-13 18:59:42 -0300

    Footer text animation

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,33 +1,70 @@
-import React from 'react'
+'use client'
+
+import React, { useRef } from 'react'
 import Button from '../Button'
 import { PiPaperPlaneTiltBold } from 'react-icons/pi'
 import Link from 'next/link'
 import Image from 'next/image'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+gsap.registerPlugin(SplitText, ScrollTrigger)
 
 const Footer = () => {
+    const container = useRef(null)
+    const linkContainer = useRef(null)
+    const title = useRef(null)
+    const links = useRef([])
+
+    useGSAP(() => {
+        const splitTitle = new SplitText(title.current, { type: 'words,lines', mask: 'lines' })
+        const splitLinks = gsap.utils.toArray(links.current).map( el => new SplitText(el, { type: 'lines', mask: 'lines'}) )
+
+        gsap.from(splitTitle.words, {
+            scrollTrigger: {
+                trigger: container.current,
+                start: 'top 50%',
+            },
+            yPercent: 100,
+            stagger: 0.05,
+            ease: 'power3.inOut'
+        })
+
+        gsap.from(splitLinks.map(s => s.lines), {
+            scrollTrigger: {
+                trigger: linkContainer.current,
+                start: 'top 90%'
+            },
+            yPercent: 100,
+            stagger: 0.05,
+            ease: 'power3.inOut',
+        })
+    }, [])
+
   return (
-    <div className='relative h-dvh overflow-hidden'>
+    <div ref={container} className='relative h-dvh overflow-hidden'>
         <div className='custom-grid h-full items-center lg:items-start'>
             <div className="col-span-3 md:col-span-7 lg:col-span-8 mt-32">
-                <span className='text-[40px] leading-none md:text-6xl lg:text-8xl font-medium'>Tudo pronto para destacar sua empresa?</span>
+                <span ref={title} className='text-[40px] leading-none md:text-6xl lg:text-8xl font-medium'>Tudo pronto para destacar sua empresa?</span>
                 <div className="flex w-dvw gap-2 lg:gap-4 mt-12">
                     <Button where="https://wa.me/message/MZ3XAEYWTNVYJ1" type='sec'>Fale Conosco <PiPaperPlaneTiltBold/></Button>
                 </div>
             </div>
 
-            <div className="col-span-4 md:col-span-7 lg:col-span-11 grid grid-cols-4 md:grid-cols-7 lg:grid-cols-11 gap-2 md:gap-4 lg:gap-8 grid-row-2 items-end">
-                <Link href='/' className="col-span-2 lg:col-span-1 row-start-1 pb-4 md:pb-0 leading-none">Voltar ao topo</Link>
+            <div ref={linkContainer} className="col-span-4 md:col-span-7 lg:col-span-11 grid grid-cols-4 md:grid-cols-7 lg:grid-cols-11 gap-2 md:gap-4 lg:gap-8 grid-row-2 items-end">
+                <Link href='/' className="col-span-2 lg:col-span-1 row-start-1 pb-4 md:pb-0 leading-none"><span ref={el => links.current[0] = el}>Voltar ao topo</span></Link>
                 <ul className='col-span-2 lg:col-start-3 lg:col-span-6 row-start-2 lg:row-start-1 grid md:hidden lg:grid gap-y-4'>
-                    <li className='col-span-1 lg:col-start-1'><Link href='/'>Sobre</Link></li>
-                    <li className='col-span-1 lg:col-start-3'><Link href='/'>Galeria</Link></li>
-                    <li className='col-span-1 lg:col-start-5'><Link href='/'>Serviços</Link></li>
+                    <li className='col-span-1 lg:col-start-1' ref={el => links.current[1] = el}><Link href='/'>Sobre</Link></li>
+                    <li className='col-span-1 lg:col-start-3' ref={el => links.current[2] = el}><Link href='/'>Galeria</Link></li>
+                    <li className='col-span-1 lg:col-start-5' ref={el => links.current[3] = el}><Link href='/'>Serviços</Link></li>
                 </ul>
                 <ul className='col-span-2 md:col-span-4 lg:col-span-1 row-start-2 md:row-start-1 lg:col-start-9 flex flex-col md:grid md:grid-cols-4 lg:flex lg:flex-col gap-y-4 md:gap-4'>
-                    <li className='col-span-1'><Link href='/'>Instagram &#8599;</Link></li>
-                    <li className='col-span-1'><Link href='/' >Dribble &#8599;</Link></li>
-                    <li className='col-span-1'><Link href='/' >Linkedin &#8599;</Link></li>
+                    <li className='col-span-1' ref={el => links.current[4] = el}><Link href='/'>Instagram &#8599;</Link></li>
+                    <li className='col-span-1' ref={el => links.current[5] = el}><Link href='/'>Dribble &#8599;</Link></li>
+                    <li className='col-span-1' ref={el => links.current[6] = el}><Link href='/'>Linkedin &#8599;</Link></li>
                 </ul>
-                <Link href='/' className='col-span-2 md:col-span-1 row-start-1 pb-4 md:pb-0 lg:col-start-11'>&#169;CtrlDev</Link>
+                <Link href='/' className='col-span-2 md:col-span-1 row-start-1 pb-4 md:pb-0 lg:col-start-11'><span ref={el => links.current[7] = el}>&#169;CtrlDev</span></Link>
             </div>
         </div>
 
